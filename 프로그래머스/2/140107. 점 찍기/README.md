c63c31a18b14593b0e6a0d785632666c00c104d5
Taek_2 <oot309@daum.net>
2024-06-23 21:04:02 +0900

    [level 2] Title: 점 찍기, Time: 0.11 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/README.md
+++ 프로그래머스/2/140107. 점 찍기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점 찍기 - 140107 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140107#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140107?language=java) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 0.08 ms
+메모리: 76.6 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 23일 21:02:18
+2024년 06월 23일 21:03:58
 
 ### 문제 설명
 
--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,22 +2,6 @@ import java.lang.Math; // Math 호출
 
 class Solution {
     public long solution(int k, int d) {
-        
-//         1차 시도 (전체 탐색) - 시간 초과
-//         long total = 0; // 총 갯수 값
-        
-//         for(int i = 0; i <= d; i += k) {
-//             for(int j = 0; j <= d; j += k) {
-//                 check = Math.sqrt(i*i + j*j);
-                
-//                 if(check > d)
-//                     break;
-                
-//                 total++;
-//             }
-//         }
-//         return total;
-        
         long total = 0;
         int yMaxDistance;
         long xx, dd;
