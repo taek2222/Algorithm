c2c27377fb70bd7ea9189fefbb57a9185b262f9e
Taek_2 <oot309@daum.net>
2024-06-23 21:25:43 +0900

    [level 2] Title: 올바른 괄호, Time: 6.57 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 52.6 MB, 시간: 7.76 ms
+메모리: 52.5 MB, 시간: 6.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 23일 21:22:27
+2024년 06월 23일 21:25:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,20 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
-        char c;
+        boolean answer = false;
         int check = 0;
 
         for(int i = 0; i < s.length(); i++) {
-            c = s.charAt(i);
             
-            if(c == '(') check++;
+            if(s.charAt(i) == '(') check++;
             else check--;
             
-            if(check < 0 || check - (s.length()-i) > 0)
-                return false;
+            if(check < 0)
+                break;
         }
         
         if(check == 0)
-            return answer;
-        else
-            return false;
+            return true;
+        
+        return answer;
     }
 }
