e6c436b4dc11c299682d8e966c1800ae34578786
Taek_2 <oot309@daum.net>
2024-06-25 22:23:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 64.56 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,26 +1,22 @@
 import java.util.*;
 
+/*
+    리팩토리 진행
+    1. 기존 코드에서 ArrayList 방법으로 변환.
+    2. stream 기능을 활용 ArrayList -> Int 배열로 전환.
+*/
 public class Solution {
     public int[] solution(int[] arr) {
-        int[] answerArray = new int[arr.length];
+        ArrayList<Integer> result = new ArrayList<>();
         
         int lastNumber = -1;
-        int i = 0;
         
-        for(int number : arr) {
-            if(lastNumber == number)
-                continue;
-            answerArray[i] = number;
-            lastNumber = number;
-            i++;
-        }
-        
-        int[] answer = new int[i];
-        
-        for(int j = 0; j < answer.length; j++)
-            answer[j] = answerArray[j];
-            
-
-        return answer;
+        for(int number : arr)
+            if(lastNumber != number) {
+                result.add(number);
+                lastNumber = number;
+            }
+    
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
