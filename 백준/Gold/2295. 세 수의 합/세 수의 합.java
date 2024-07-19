f4c6449e37b1e80d6f035720cda44d358229926a
Taek_2 <oot309@daum.net>
2024-07-19 17:42:50 +0900

    [Gold IV] Title: 세 수의 합, Time: 440 ms, Memory: 57868 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
+/** 리팩토링 #1
  * 예제 입력
  * 5
  * 2
@@ -31,16 +32,15 @@ public class Main {
             for(int num2 : numberArray)
                 numbersSum.add(num1+num2);
 
-        int max = 0;
+        numberArray.sort(Collections.reverseOrder());
 
         for(int num1 : numberArray)
             for (int num2 : numberArray) {
                 int searchNumber = num1 - num2;
-                if (numbersSum.contains(searchNumber))
-                    if(max < num1)
-                        max = num1;
+                if (numbersSum.contains(searchNumber)) {
+                    System.out.println(num1);
+                    System.exit(0);
+                }
             }
-
-        System.out.println(max);
     }
 }
