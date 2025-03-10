e4bfa33d5fe353838e8579fff58161f262796537
Taek_2 <oot309@daum.net>
2025-03-10 22:56:20 +0900

    [Silver V] Title: 단어 정렬, Time: 572 ms, Memory: 26920 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] str = new String[Integer.parseInt(br.readLine())];
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < str.length; i++)
-            str[i] = br.readLine();
-
-        for(int i = 0; i < (str.length -1) ; i++) {
-            int check = 0;
-            for(int j = i+1; j < (str.length); j++) {
-                if(str[i].length() > str[j].length()){
-                    String temp = str[i];
-                    str[i] = str[j];
-                    str[j] = temp;
-                    check = 1;
-                } else if(str[i].length() == str[j].length()) {
-                    if(str[i].compareTo(str[j]) > 0) {
-                        String temp = str[i];
-                        str[i] = str[j];
-                        str[j] = temp;
-                        check = 1;
-                    }
-                }
-                if(str[i].equals(str[j])) {
-                    check = 2;
-                    break;
-                }
-            }
-            if(check == 1)
-                System.out.println(str[i]);
+        Set<String> inputs = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            inputs.add(br.readLine());
         }
-        System.out.print(str[str.length-1]);
+
+        List<String> sorted = inputs.stream()
+                .sorted(Comparator.comparing(String::length).thenComparing(String::compareTo))
+                .collect(Collectors.toList());
+
+        for (String element : sorted) {
+            System.out.println(element);
+        }
     }
 }
