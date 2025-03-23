3807c86ad62f069173e856f0e092eb229c13f617
Taek_2 <oot309@daum.net>
2025-03-23 16:33:37 +0900

    [Silver V] Title: 덩치, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,28 +1,77 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+
+    static class People implements Comparable {
+        private final int weight;
+        private final int height;
+
+        public People(final int weight, final int height) {
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public int comparison(final People other) {
+            if (this.compareTo(other) < 0) {
+                return 1;
+            }
+            return 0;
+        }
+
+        @Override
+        public int compareTo(final Object o) {
+            final People other = (People) o;
+
+            if (this.height > other.height && this.weight < other.weight) {
+                return 0;
+            }
+
+            if (this.height < other.height && this.weight > other.weight) {
+                return 0;
+            }
+
+            if (this.height < other.height && this.weight < other.weight) {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder str = new StringBuilder();
-        int[][] size = new int[Integer.parseInt(br.readLine())][2];
-        for (int i = 0; i < size.length; i++) {
-            StringTokenizer WH = new StringTokenizer(br.readLine());
-            size[i][0] = Integer.parseInt(WH.nextToken());
-            size[i][1] = Integer.parseInt(WH.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<People> peoples = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            People people = new People(
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())
+            );
+            peoples.add(people);
         }
-        for(int i = 0; i < size.length; i++) {
-            int weight = size[i][0];
-            int height = size[i][1];
-            int k = 0;
-            for(int j = 0; j < size.length; j++) {
-                if(weight < size[j][0] && height < size[j][1])
-                    k++;
+
+        for (int i = 0; i < peoples.size(); i++) {
+            People comparePeople = peoples.get(i);
+            int rank = 1;
+            for (int j = 0; j < peoples.size(); j++) {
+                int comparison = comparePeople.comparison(peoples.get(j));
+                rank += comparison;
             }
-            str.append(k+1).append(" ");
+            bw.append(String.valueOf(rank)).append(" ");
         }
-        System.out.print(str);
+
+        br.close();
+        bw.close();
     }
 }
