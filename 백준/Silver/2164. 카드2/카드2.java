13d77daf0d8c9f21a1e187925fd58875fffcf8bc
Taek_2 <oot309@daum.net>
2025-03-28 23:50:08 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 24816 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> number = new ArrayList<>();
+
+        int range = Integer.parseInt(br.readLine());
+
+        if(range % 2 == 0) {
+            for (int i = 2; i <= range; i += 2)
+                number.add(i);
+        }
+        else {
+            number.add(range);
+            for (int i = 2; i < range; i += 2)
+                number.add(i);
+        }    
+
+        for (int i = 2; i < number.size(); i += 2) {
+            number.add(number.get(i-1));
+        }
+        System.out.println(number.get(number.size()-1));
+    }
+}
