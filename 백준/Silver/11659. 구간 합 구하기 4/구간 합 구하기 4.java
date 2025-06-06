39b8d8eee13cc8ea01d4432381b463a2eafbc5ed
Taek_2 <oot309@daum.net>
2025-06-06 14:50:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 500 ms, Memory: 53204 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] cumulativeSum = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            cumulativeSum[i] = cumulativeSum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            bw.append(String.valueOf(cumulativeSum[y] - cumulativeSum[x - 1])).append("\n");
+        }
+
+        bw.flush();
+    }
+}
