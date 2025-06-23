73c73963df36450586fbe3941a20accf7102fcea
Taek_2 <oot309@daum.net>
2025-06-23 14:20:43 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20032 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,47 +4,33 @@ import java.io.InputStreamReader;
 
 class Main {
 
-    static String S;
-    static String P;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        S = br.readLine();
-
-        int oCount = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; oCount < N; i++) {
-            if (i % 2 == 0) {
-                sb.append("I");
-            } else {
-                sb.append("O");
-                oCount++;
-            }
-        }
-        P = sb.append("I").toString();
+        String S = br.readLine();
 
         int answer = 0;
-        for (int i = 0; i < M; i++) {
-            if (P.charAt(0) == S.charAt(i)) {
-                if (search(i + 1)) {
-                    answer++;
-                    i += 1;
+        int count = 0;
+        for (int i = 0; i < M - 1; i++) {
+
+            if (S.charAt(i) == 'O' && S.charAt(i + 1) == 'I') {
+                count++;
+
+                if (count == N) {
+                    int index = i - (2 * count - 1);
+                    if (index >= 0 && S.charAt(index) == 'I') {
+                        answer++;
+                    }
+                    count--;
                 }
+                i += 1;
+            } else {
+                count = 0;
             }
         }
 
         System.out.println(answer);
-    }
-
-    private static boolean search(int fromIndex) {
-        for (int i = 1; i < P.length(); i++, fromIndex++) {
-            if (fromIndex >= S.length() || P.charAt(i) != S.charAt(fromIndex)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
