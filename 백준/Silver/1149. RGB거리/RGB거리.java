f687e060f4d7dd596ec84d271901fa4cbf90ce8c
Taek_2 <oot309@daum.net>
2025-06-29 13:23:58 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[N + 1][3];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int R = Integer.parseInt(st.nextToken());
+            int G = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + R;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + G;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + B;
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            answer = Math.min(dp[N][i], answer);
+        }
+
+        System.out.println(answer);
+    }
+}
