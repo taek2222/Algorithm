06b584049a333b00e1e7285cc61845ea0d8e9cb2
Taek_2 <oot309@daum.net>
2025-07-02 16:16:19 +0900

    [Gold II] Title: 트리의 지름, Time: 1260 ms, Memory: 119072 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static Map<Integer, List<Node>> nodes;
+    static int V;
+    static boolean[] visit;
+    static int max;
+    static int lastNode;
+
+    static class Node {
+        private int end;
+        private int value;
+
+        public Node(final int end, final int value) {
+            this.end = end;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        V = Integer.parseInt(br.readLine());
+
+        nodes = new HashMap<>();
+        for (int i = 0; i < V; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            while (true) {
+                int end = Integer.parseInt(st.nextToken());
+                if (end == -1) {
+                    break;
+                }
+                int value = Integer.parseInt(st.nextToken());
+                List<Node> values = nodes.getOrDefault(start, new ArrayList<>());
+                values.add(new Node(end, value));
+                nodes.put(start, values);
+            }
+        }
+
+        max = 0;
+        visit = new boolean[V + 1];
+        visit[1] = true;
+        dfs(1, 0);
+
+        visit = new boolean[V + 1];
+        visit[lastNode] = true;
+        dfs(lastNode, 0);
+
+        System.out.println(max);
+    }
+
+    private static void dfs(int pos, int len) {
+        if (max < len) {
+            lastNode = pos;
+            max = len;
+        }
+
+        List<Node> nodeList = nodes.getOrDefault(pos, new ArrayList<>());
+        for (Node node : nodeList) {
+            if (visit[node.end]) {
+                continue;
+            }
+            visit[node.end] = true;
+            dfs(node.end, len + node.value);
+        }
+    }
+}
