b598a4c7b88db48889abd0724766542da90a8ceb
Taek_2 <oot309@daum.net>
2025-07-13 17:52:45 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 144 ms, Memory: 14872 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] numbers = new int[N];
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = numbers[i];
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] <= numbers[j]) {
+                    continue;
+                }
+                dp[i] = Math.max(dp[i], dp[j] + numbers[i]);
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
