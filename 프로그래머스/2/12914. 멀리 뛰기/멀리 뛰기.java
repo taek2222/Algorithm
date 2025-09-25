be3c668a94461341511a31e9b6eea4ba4fc67f82
Taek_2 <oot309@daum.net>
2025-09-26 00:16:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[n + 2];
+        
+        dp[1] = 1L;
+        dp[2] = 2L;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
