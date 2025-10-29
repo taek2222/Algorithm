b532f68b6a119a821d4ef83ac825f79d673a7edb
Taek_2 <oot309@daum.net>
2025-10-30 00:30:21 +0900

    [level 3] Title: 기지국 설치, Time: 3.37 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -2,22 +2,22 @@ class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
         int coverage = w * 2 + 1;
-        int prevend = 0;
+        int spread = 0;
         
         for(int station : stations) {
             int start = Math.max(1, station - w);
             int end = Math.min(n, station + w);
             
-            if(start > prevend + 1) {
-                int gap = start - prevend - 1;
+            if(start > spread) {
+                int gap = start - spread - 1;
                 answer += Math.ceil((double) gap / coverage);
             }
             
-            prevend = end;
+            spread = end;
         }
         
-        if(prevend < n) {
-            int gap = n - prevend;
+        if(spread < n) {
+            int gap = n - spread;
             answer += Math.ceil((double) gap / coverage);
         }
 
