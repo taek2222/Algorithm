0574d94f2b1442f238132346e3de21c47224b9e2
Taek_2 <oot309@daum.net>
2025-11-10 13:17:56 +0900

    [level 2] Title: 가장 큰 수, Time: 151.85 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,17 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] strings = Arrays.stream(numbers)
-                                    .mapToObj(String::valueOf)
-                                    .toArray(String[]::new);
+        List<String> nums = new ArrayList<>();
+        for(int number : numbers) {
+            nums.add(String.valueOf(number));
+        }
         
-        Arrays.sort(strings, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Collections.sort(nums, (n1, n2) -> {
+            return (n2 + n1).compareTo(n1 + n2);
+        });
         
-        if(strings[0].equals("0")) {
+        if(nums.get(0).equals("0")) {
             return "0";
         }
         
-        String answer = String.join("", strings);
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(String number : nums) {
+            sb.append(number);
+        }
+        
+        return sb.toString();
     }
 }
+
+/**
+1. 문자열로 전부 치환
+2. 정렬 후 하나씩
+**/
