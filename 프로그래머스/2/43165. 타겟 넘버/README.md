577bf67be8b3330c0c2c50e32f87960f8def984e
Taek_2 <oot309@daum.net>
2025-11-10 10:17:40 +0900

    [level 2] Title: 타겟 넘버, Time: 8.98 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 7.61 ms
+메모리: 74.2 MB, 시간: 8.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 31일 23:57:06
+2025년 11월 10일 10:17:38
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,26 @@
 class Solution {
     
     int answer = 0;
-    
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        dfs(0, numbers, 0, target);
         return answer;
     }
     
-    private void dfs(int[] numbers, int target, int num, int index) {
-        if(index == numbers.length) {
-            if (num == target) {
+    public void dfs(int depth, int[] numbers, int current, int target) {
+        if(depth == numbers.length) {
+            if(current == target) {
                 answer++;
             }
             return;
         }
         
-        dfs(numbers, target, num + numbers[index], index + 1);
-        dfs(numbers, target, num - numbers[index], index + 1);
+        int num = numbers[depth];
+        dfs(depth + 1, numbers, current + num, target);
+        dfs(depth + 1, numbers, current - num, target);
     }
 }
+
+/**
+1. DFS, 방문 처리는 할 필요 없음
+2. 깊이에 따라 마지막 체크
+**/
