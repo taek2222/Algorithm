bbbaf86b59ce1f69fbb22f6f0b04ae44278ac528
Taek_2 <oot309@daum.net>
2025-11-10 14:08:17 +0900

    [level 2] Title: 문자열 압축, Time: 5.99 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,41 +1,48 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
+        int answer = Integer.MAX_VALUE;
         
         for(int i = 1; i <= s.length() / 2; i++) {
             StringBuilder sb = new StringBuilder();
-            String prev = s.substring(0, i);
+            String sub = s.substring(0, i);
             int count = 1;
-            
-            for(int j = i; j + i <= s.length(); j += i) {
+            for(int j = i; j <= s.length(); j += i) {
                 String next;
-                if (j + i <= s.length()) {
+                if(j + i <= s.length()) {
                     next = s.substring(j, j + i);
                 } else {
                     next = s.substring(j);
                 }
                 
-                if(prev.equals(next)) {
+                if(next.equals(sub)) {
                     count++;
+                    continue;
                 } else {
-                    if(count > 1) sb.append(count);
-                    sb.append(prev);
-                    prev = next;
+                    if(count > 1) {
+                        sb.append(count);
+                    }
+                    sb.append(sub);
+                    sub = next;
                     count = 1;
                 }
             }
-            if(count > 1) sb.append(count);
-            sb.append(prev);
-            
-            if(s.length() % i != 0) {
-                sb.append(s.substring(s.length() - s.length() % i));
+            if(count > 1) {
+                sb.append(count);
             }
+            sb.append(sub);
             
             answer = Math.min(answer, sb.length());
         }
         
+        if(s.length() == 1) {
+            return 1;
+        }
         return answer;
     }
 }
+
+/**
+1. 전체 길이의 반까지 시도
+2. 반복문 하면서 넘어가고
+3. 가장 작은 수 
+**/
