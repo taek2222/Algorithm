fc6e47e03ad59c3b3804d34c7fc01269900dde91
Taek_2 <oot309@daum.net>
2025-11-10 11:09:52 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,26 @@
-import java.util.*;
-
 class Solution {
+    
+    boolean[] visit;
     public int solution(int n, int[][] computers) {
-        boolean[] visit = new boolean[n];
+        visit = new boolean[computers.length];
         int answer = 0;
         
         for(int i = 0; i < n; i++) {
-            if(visit[i])
-                continue;
-            
-            answer++;
-            
-            ArrayDeque<Integer> queue = new ArrayDeque<>();
-            queue.add(i);
+            if(visit[i]) continue;
             visit[i] = true;
-            
-            while(!queue.isEmpty()) {
-                int nowCom = queue.poll();
-            
-                for(int j = 0; j < computers[i].length; j++) {
-                    if(nowCom == j || visit[j] || computers[nowCom][j] == 0)
-                        continue;
-                    
-                    visit[j] = true;
-                    queue.add(j);
-                }
-            }
+            dfs(i, computers);
+            answer++;
         }
-        
         return answer;
+    }
+    
+    public void dfs(int pos, int[][] computers) {
+        for(int i = 0; i < computers[pos].length; i++) {
+            if(computers[pos][i] != 1 || visit[i]) {
+                continue;
+            }
+            visit[i] = true;
+            dfs(i, computers);
+        }
     }
 }
