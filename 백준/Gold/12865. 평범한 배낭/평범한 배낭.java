ee938c56149a2592dac32ecd74dcc6934c205010
Taek_2 <oot309@daum.net>
2025-12-10 23:50:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] weight = new int[N + 1];
+        int[] value = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            int w = weight[i];
+            int v = value[i];
+            for (int j = K; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j - w] + v);
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}
